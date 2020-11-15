39e0324319d8520263265c05ada1388494983080
Lurzapps <lurzapps@gmail.com>
2020-11-15 23:18:25 +0100

    update

--- lib/chess_controller.dart
+++ lib/chess_controller.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:chess_bot/chess_board/chess.dart';
 import 'package:chess_bot/chess_board/src/chess_sub.dart' as chess;
+import 'package:chess_bot/main.dart';
 import 'package:chess_bot/utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,8 @@ class ChessController {
   ChessBoardController controller;
   Chess game;
   BuildContext context;
+  bool _showing = false;
+  bool hasReadGameState = false;
 
   ChessController(this.context);
 
@@ -19,17 +22,66 @@ class ChessController {
     print('onMove: $move');
     //the piece
     chess.Piece piece = game.get(move['square']);
+
   }
 
   void onDraw() {
     print('onDraw');
+
   }
 
-  void onCheckMate(color) {
-    print('onCheckMate: $color');
+  void onCheckMate(color) async {
+    if(_showing)
+      return;
+
+    //determine winner and loser
+    var winner = color == PieceColor.White ? strings.black : strings.white;
+    var loser = color == PieceColor.White ? strings.white : strings.black;
+    //show dialog
+    (await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(
+              child: Text(
+                  strings.checkmate
+              )
+          ),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children : <Widget>[
+              Expanded(
+                child: Text(
+                  strings.check_mate_desc(loser, winner),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+
+                    color: Colors.red,
+                  ),
+                ),
+              )
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+                child: Text(strings.cancel),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                }),
+            FlatButton(
+                child: Text(strings.replay),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                })
+          ],
+        );
+      },
+    )).then((value) => _showing = false);
   }
 
-  void onCheck() {
+  void onCheck(color) {
     print('onCheck');
   }
 
@@ -42,6 +94,8 @@ class ChessController {
         return Chess.DEFAULT_POSITION;
 
       print('game loaded');
+
+      hasReadGameState = true;
 
       return fen;
     }
