551afdaad15cb8035029b09832e86ae37651af6a
Lurzapps <lurzapps@gmail.com>
2020-11-19 21:05:56 +0100

    update

--- lib/ai.dart
+++ lib/ai.dart
@@ -132,7 +132,7 @@ class ChessAI {
       }
     } else {
       // otherwise do a simple material evaluation
-      double evaluation = 0.0;
+      /*double evaluation = 0.0;
       for (int i = Chess.SQUARES_A8; i <= Chess.SQUARES_H1; i++) {
         if ((i & 0x88) != 0) {
           i += 7;
@@ -145,26 +145,41 @@ class ChessAI {
               ? _pieceValues[piece.type]
               : -_pieceValues[piece.type];
         }
+      }*/
+
+      double eval = 0.0;
+      for (int i = Chess.SQUARES_A8; i <= Chess.SQUARES_H1; i++) {
+        if ((i & 0x88) != 0) {
+          i += 7;
+          continue;
+        }
+
+        Piece piece = c.game.board[i];
+        if (piece != null) {
+          //get the x and y from the map
+          final xAndY = _COORDINATES_SQUARES[i];
+          //evaluate the piece at the position
+          eval += _getPieceValue(piece, xAndY[0], xAndY[1]);
+        }
       }
 
-      return evaluation;
+      return eval;
     }
   }
 
-  // the evaluation function for minimax
-  static int _evaluateBoard(board) {
-    int totalEvaluation = 0;
-
-    for (var i = 0; i < 8; i++) {
-      for (var j = 0; j < 8; j++) {
-        totalEvaluation = totalEvaluation + _getPieceValue(board[i][j], i, j);
-      }
-    }
-    return totalEvaluation;
-  }
+  static const Map _COORDINATES_SQUARES = const {
+    0: [0, 7], 1: [1, 7], 2: [2, 7], 3: [3, 7], 4: [4, 7], 5 : [5, 7], 6 : [6, 7], 7 : [7, 7],
+    16: [0, 6], 17: [1, 6], 18: [2, 6], 19: [3, 6], 20: [4, 6], 21: [5, 6], 22: [6, 6], 23: [7, 6],
+    32: [0, 5], 33: [1, 5], 34: [2, 5], 35: [3, 5], 36: [4, 5], 37: [5, 5], 38: [6, 5], 39: [7, 5],
+    48: [0, 4], 49: [1, 4], 50: [2, 4], 51: [3, 4], 52: [4, 4], 53: [5, 4], 54: [6, 4], 55: [7, 4],
+    64: [0, 3], 65: [1, 3], 66: [2, 3], 67: [3, 3], 68: [4, 3], 69: [5, 3], 70: [6, 3], 71: [7, 3],
+    80: [0, 2], 81: [1, 2], 82: [2, 2], 83: [3, 2], 84: [4, 2], 85: [5, 2], 86: [6, 2], 87: [7, 2],
+    96: [0, 1], 97: [1, 1], 98: [2, 1], 99: [3, 1], 100: [4, 1], 101: [5, 1], 102: [6, 1], 103: [7, 1],
+    112: [0, 0], 113: [1, 0], 114: [2, 0], 115: [3, 0], 116: [4, 0], 117: [5, 0], 118: [6, 0], 119: [7, 0]
+  };
 
   static List _reverseList(List list) {
-    return [...list].reversed;
+    return [...list].reversed.toList();
   }
 
   static const _whitePawnEval = [
@@ -241,32 +256,33 @@ class ChessAI {
 
   static final _blackKingEval = _reverseList(_whiteKingEval);
 
-  static _getPieceValue(piece, x, y) {
+  static double _getPieceValue(Piece piece, int x, int y) {
     if (piece == null) {
       return 0;
     }
 
-    var absoluteValue = _getAbsoluteValue(piece, piece.color == 'w', x, y);
+    var absoluteValue =
+        _getAbsoluteValue(piece.type, piece.color == Color.WHITE, x, y);
 
-    if (piece.color == 'w') {
+    if (piece.color == _MAX) {
       return absoluteValue;
     } else {
       return -absoluteValue;
     }
   }
 
-  static double _getAbsoluteValue(piece, isWhite, x, y) {
-    if (piece.type == 'p') {
+  static double _getAbsoluteValue(PieceType piece, bool isWhite, int x, int y) {
+    if (piece.name == 'p') {
       return 10 + (isWhite ? _whitePawnEval[y][x] : _blackPawnEval[y][x]);
-    } else if (piece.type == 'r') {
+    } else if (piece.name == 'r') {
       return 50 + (isWhite ? _whiteRookEval[y][x] : _blackRookEval[y][x]);
-    } else if (piece.type == 'n') {
+    } else if (piece.name == 'n') {
       return 30 + _knightEval[y][x];
-    } else if (piece.type == 'b') {
+    } else if (piece.name == 'b') {
       return 30 + (isWhite ? _whiteBishopEval[y][x] : _blackBishopEval[y][x]);
-    } else if (piece.type == 'q') {
+    } else if (piece.name == 'q') {
       return 90 + _evalQueen[y][x];
-    } else if (piece.type == 'k') {
+    } else if (piece.name == 'k') {
       return 900 + (isWhite ? _whiteKingEval[y][x] : _blackKingEval[y][x]);
     }
 
