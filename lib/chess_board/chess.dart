f27b12c4c2d80a5d3ce91b047cf994cb60eb4dc6
Lurzapps <lurzapps@gmail.com>
2020-11-24 23:27:10 +0100

    update

--- lib/chess_board/chess.dart
+++ lib/chess_board/chess.dart
@@ -1,7 +1,6 @@
 library chess;
 
 import 'package:chess_bot/chess_board/src/chess_sub.dart';
-import 'package:uuid/uuid.dart';
 
 /*  Copyright (c) 2014, David Kopec (my first name at oaksnow dot com)
  *  Released under the MIT license
@@ -21,8 +20,8 @@ class Chess {
 
   //this prevents calling generate_moves for the same move multiple times
   Map generatedMoves = {
-    'gen_uuid': uuid.v4(),
-    'move_uuid': 'sth_different',
+    'gen_move': null,
+    'move_move': null,
     'gen': <Move>[]
   };
 
@@ -43,9 +42,9 @@ class Chess {
       ..game.kings = new ColorMap.clone(this.game.kings)
       ..game.turn = new Color.fromInt(this.game.turn.value)
       ..game.castling = new ColorMap.clone(this.game.castling)
-      ..game.ep_square = this.game.ep_square
-      ..game.half_moves = this.game.half_moves
-      ..game.move_number = this.game.move_number
+      ..game.epSquare = this.game.epSquare
+      ..game.halfMoves = this.game.halfMoves
+      ..game.moveNumber = this.game.moveNumber
       ..game.history = new List<State>.from(this.game.history)
       ..game.header = new Map.from(this.game.header);
   }
@@ -64,8 +63,8 @@ class Chess {
 
   void resetGeneratedMovesMap() {
     generatedMoves = {
-      'gen_uuid': uuid.v4(),
-      'move_uuid': 'sth_different',
+      'gen_move': null,
+      'move_move': null,
       'gen': <Move>[]
     };
   }
@@ -119,9 +118,9 @@ class Chess {
       game.castling[BLACK] |= BITS_QSIDE_CASTLE;
     }
 
-    game.ep_square = (tokens[3] == '-') ? EMPTY : SQUARES[tokens[3]];
-    game.half_moves = int.parse(tokens[4]);
-    game.move_number = int.parse(tokens[5]);
+    game.epSquare = (tokens[3] == '-') ? EMPTY : SQUARES[tokens[3]];
+    game.halfMoves = int.parse(tokens[4]);
+    game.moveNumber = int.parse(tokens[5]);
 
     update_setup(generate_fen());
 
@@ -280,9 +279,9 @@ class Chess {
       cflags = '-';
     }
     String epflags =
-        (game.ep_square == EMPTY) ? '-' : algebraic(game.ep_square);
+        (game.epSquare == EMPTY) ? '-' : algebraic(game.epSquare);
 
-    return [fen, game.turn, cflags, epflags, game.half_moves, game.move_number]
+    return [fen, game.turn, cflags, epflags, game.halfMoves, game.moveNumber]
         .join(' ');
   }
 
@@ -372,9 +371,11 @@ class Chess {
         game.turn, from, to, flags, board[from].type, captured, promotion);
   }
 
-  List<Move> generate_moves([Map options]) {
-    if (generatedMoves['move_uuid'] == generatedMoves['gen_uuid'])
+  List<Move> generateMoves([Map options]) {
+    if ((generatedMoves['move_move'] == generatedMoves['gen_move']) && (generatedMoves['gen'].length != 0)) {
+      print('saved');
       return generatedMoves['gen'];
+    }
 
     // ignore: non_constant_identifier_names
     void add_move(List<Move> moves, from, to, flags) {
@@ -449,8 +450,8 @@ class Chess {
 
           if (game.board[square] != null && game.board[square].color == them) {
             add_move(moves, i, square, BITS_CAPTURE);
-          } else if (square == game.ep_square) {
-            add_move(moves, i, game.ep_square, BITS_EP_CAPTURE);
+          } else if (square == game.epSquare) {
+            add_move(moves, i, game.epSquare, BITS_EP_CAPTURE);
           }
         }
       } else {
@@ -526,10 +527,10 @@ class Chess {
       if (!king_attacked(us)) {
         legal_moves.add(moves[i]);
       }
-      undo_move();
+      undo();
     }
 
-    generatedMoves['gen_uuid'] = generatedMoves['move_uuid'];
+    generatedMoves['gen_move'] = generatedMoves['move_move'];
     return generatedMoves['gen'] = legal_moves;
   }
 
@@ -570,7 +571,7 @@ class Chess {
         output += '+';
       }
     }
-    undo_move();
+    undo();
 
     return output;
   }
@@ -632,11 +633,11 @@ class Chess {
   }
 
   bool get in_checkmate {
-    return in_check && generate_moves().length == 0;
+    return in_check && generateMoves().length == 0;
   }
 
   bool get in_stalemate {
-    return !in_check && generate_moves().length == 0;
+    return !in_check && generateMoves().length == 0;
   }
 
   bool get insufficient_material {
@@ -692,12 +693,13 @@ class Chess {
      * Zobrist key would be maintained in the make_move/undo_move functions,
      * avoiding the costly that we do below.
      */
+
     List moves = [];
     Map positions = {};
     bool repetition = false;
 
     while (true) {
-      var move = undo_move();
+      var move = undo();
       if (move == null) {
         break;
       }
@@ -730,9 +732,9 @@ class Chess {
         new ColorMap.clone(game.kings),
         game.turn,
         new ColorMap.clone(game.castling),
-        game.ep_square,
-        game.half_moves,
-        game.move_number));
+        game.epSquare,
+        game.halfMoves,
+        game.moveNumber));
   }
 
   void make_move(Move move) {
@@ -754,7 +756,7 @@ class Chess {
 
     /* if pawn promotion, replace with new piece */
     if ((move.flags & BITS_PROMOTION) != 0) {
-      game.board[move.to] = new Piece(move.promotion, us);
+      game.board[move.to] = Piece(move.promotion, us);
     }
 
     /* if we moved the king */
@@ -803,33 +805,33 @@ class Chess {
     /* if big pawn move, update the en passant square */
     if ((move.flags & BITS_BIG_PAWN) != 0) {
       if (game.turn == BLACK) {
-        game.ep_square = move.to - 16;
+        game.epSquare = move.to - 16;
       } else {
-        game.ep_square = move.to + 16;
+        game.epSquare = move.to + 16;
       }
     } else {
-      game.ep_square = EMPTY;
+      game.epSquare = EMPTY;
     }
 
     /* reset the 50 move counter if a pawn is moved or a piece is captured */
     if (move.piece == PAWN) {
-      game.half_moves = 0;
+      game.halfMoves = 0;
     } else if ((move.flags & (BITS_CAPTURE | BITS_EP_CAPTURE)) != 0) {
-      game.half_moves = 0;
+      game.halfMoves = 0;
     } else {
-      game.half_moves++;
+      game.halfMoves++;
     }
 
     if (game.turn == BLACK) {
-      game.move_number++;
+      game.moveNumber++;
     }
     game.turn = swap_color(game.turn);
 
-    generatedMoves['move_uuid'] = uuid.v4();
+    generatedMoves['move_uuid'] = move;
   }
 
   /// Undoes a move and returns it, or null if move history is empty
-  Move undo_move() {
+  Move undo() {
     if (game.history.isEmpty) {
       return null;
     }
@@ -842,9 +844,9 @@ class Chess {
     game.kings = old.kings;
     game.turn = old.turn;
     game.castling = old.castling;
-    game.ep_square = old.ep_square;
-    game.half_moves = old.half_moves;
-    game.move_number = old.move_number;
+    game.epSquare = old.epSquare;
+    game.halfMoves = old.halfMoves;
+    game.moveNumber = old.moveNumber;
 
     Color us = game.turn;
     Color them = swap_color(game.turn);
@@ -884,7 +886,7 @@ class Chess {
 
   /* this function is used to uniquely identify ambiguous moves */
   get_disambiguator(Move move) {
-    List<Move> moves = generate_moves();
+    List<Move> moves = generateMoves();
 
     var from = move.from;
     var to = move.to;
@@ -1029,82 +1031,8 @@ class Chess {
     return str.replaceAll(new RegExp(r"^\s+|\s+$"), '');
   }
 
-  // debug utility
-  perft(int depth) {
-    List<Move> moves = generate_moves({'legal': false});
-    var nodes = 0;
-    var color = game.turn;
-
-    for (var i = 0, len = moves.length; i < len; i++) {
-      make_move(moves[i]);
-      if (!king_attacked(color)) {
-        if (depth - 1 > 0) {
-          var child_nodes = perft(depth - 1);
-          nodes += child_nodes;
-        } else {
-          nodes++;
-        }
-      }
-      undo_move();
-    }
-
-    return nodes;
-  }
-
-  //Public APIs
-
-  ///  Returns a list of legals moves from the current position.
-  ///  The function takes an optional parameter which controls the
-  ///  single-square move generation and verbosity.
-  ///
-  ///  The piece, captured, and promotion fields contain the lowercase
-  ///  representation of the applicable piece.
-  ///
-  ///  The flags field in verbose mode may contain one or more of the following values:
-  ///
-  ///  'n' - a non-capture
-  ///  'b' - a pawn push of two squares
-  ///  'e' - an en passant capture
-  ///  'c' - a standard capture
-  ///  'p' - a promotion
-  ///  'k' - kingside castling
-  ///  'q' - queenside castling
-  ///  A flag of 'pc' would mean that a pawn captured a piece on the 8th rank and promoted.
-  ///
-  ///  If "asObjects" is set to true in the options Map, then it returns a List<Move>
-  List moves([Map options]) {
-    /* The internal representation of a chess move is in 0x88 format, and
-       * not meant to be human-readable.  The code below converts the 0x88
-       * square coordinates to algebraic coordinates.  It also prunes an
-       * unnecessary move keys resulting from a verbose call.
-       */
-
-    List<Move> ugly_moves = generate_moves(options);
-    if (options != null &&
-        options.containsKey('asObjects') &&
-        options['asObjects'] == true) {
-      return ugly_moves;
-    }
-    List moves = [];
-
-    for (int i = 0, len = ugly_moves.length; i < len; i++) {
-      /* does the user want a full move object (most likely not), or just
-         * SAN
-         */
-      if (options != null &&
-          options.containsKey('verbose') &&
-          options['verbose'] == true) {
-        moves.add(make_pretty(ugly_moves[i]));
-      } else {
-        moves.add(move_to_san(ugly_moves[i]));
-      }
-    }
-
-    return moves;
-  }
-
   bool get in_draw {
-    return game.half_moves >= 100 ||
+    return game.halfMoves >= 100 ||
         in_stalemate ||
         insufficient_material ||
         in_threefold_repetition;
@@ -1118,255 +1046,6 @@ class Chess {
     return generate_fen();
   }
 
-  /// Return the PGN representation of the game thus far
-  pgn([Map options]) {
-    /* using the specification from http://www.chessclub.com/help/PGN-spec
-       * example for html usage: .pgn({ max_width: 72, newline_char: "<br />" })
-       */
-    var newline = (options != null &&
-            options.containsKey("newline_char") &&
-            options["newline_char"] != null)
-        ? options['newline_char']
-        : '\n';
-    var max_width = (options != null &&
-            options.containsKey("max_width") &&
-            options["max_width"] != null)
-        ? options["max_width"]
-        : 0;
-    var result = [];
-    var header_exists = false;
-
-    /* add the PGN header headerrmation */
-    for (var i in game.header.keys) {
-      /* TODO: order of enumerated properties in header object is not
-         * guaranteed, see ECMA-262 spec (section 12.6.4)
-         */
-      result.add('[' +
-          i.toString() +
-          ' \"' +
-          game.header[i].toString() +
-          '\"]' +
-          newline);
-      header_exists = true;
-    }
-
-    if (header_exists && (game.history.length != 0)) {
-      result.add(newline);
-    }
-
-    /* pop all of history onto reversed_history */
-    List<Move> reversed_history = [];
-    while (game.history.length > 0) {
-      reversed_history.add(undo_move());
-    }
-
-    List<String> moves = [];
-    String move_string = '';
-    int pgn_move_number = 1;
-
-    /* build the list of moves.  a move_string looks like: "3. e3 e6" */
-    while (reversed_history.length > 0) {
-      Move move = reversed_history.removeLast();
-
-      /* if the position started with black to move, start PGN with 1. ... */
-      if (pgn_move_number == 1 && move.color == BLACK) {
-        move_string = '1. ...';
-        pgn_move_number++;
-      } else if (move.color == WHITE) {
-        /* store the previous generated move_string if we have one */
-        if (move_string.length != 0) {
-          moves.add(move_string);
-        }
-        move_string = pgn_move_number.toString() + '.';
-        pgn_move_number++;
-      }
-
-      move_string = move_string + ' ' + move_to_san(move);
-      make_move(move);
-    }
-
-    /* are there any other leftover moves? */
-    if (move_string.length != 0) {
-      moves.add(move_string);
-    }
-
-    /* is there a result? */
-    if (game.header['Result'] != null) {
-      moves.add(game.header['Result']);
-    }
-
-    /* history should be back to what is was before we started generating PGN,
-       * so join together moves
-       */
-    if (max_width == 0) {
-      return result.join('') + moves.join(' ');
-    }
-
-    /* wrap the PGN output at max_width */
-    var current_width = 0;
-    for (int i = 0; i < moves.length; i++) {
-      /* if the current move will push past max_width */
-      if (current_width + moves[i].length > max_width && i != 0) {
-        /* don't end the line with whitespace */
-        if (result[result.length - 1] == ' ') {
-          result.removeLast();
-        }
-
-        result.add(newline);
-        current_width = 0;
-      } else if (i != 0) {
-        result.add(' ');
-        current_width++;
-      }
-      result.add(moves[i]);
-      current_width += moves[i].length;
-    }
-
-    return result.join('');
-  }
-
-  /// Load the moves of a game stored in Portable Game Notation.
-  /// [options] is an optional parameter that contains a 'newline_char'
-  /// which is a string representation of a RegExp (and should not be pre-escaped)
-  /// and defaults to '\r?\n').
-  /// Returns [true] if the PGN was parsed successfully, otherwise [false].
-  load_pgn(String pgn, [Map options]) {
-    mask(str) {
-      return str.replaceAll(new RegExp(r"\\"), '\\');
-    }
-
-    /* convert a move from Standard Algebraic Notation (SAN) to 0x88
-       * coordinates
-      */
-    move_from_san(move) {
-      var moves = generate_moves();
-      for (var i = 0, len = moves.length; i < len; i++) {
-        /* strip off any trailing move decorations: e.g Nf3+?! */
-        if (move.replaceAll(new RegExp(r"[+#?!=]+$"), '') ==
-            move_to_san(moves[i]).replaceAll(new RegExp(r"[+#?!=]+$"), '')) {
-          return moves[i];
-        }
-      }
-      return null;
-    }
-
-    get_move_obj(move) {
-      return move_from_san(trim(move));
-    }
-
-    /*has_keys(object) {
-        bool has_keys = false;
-        for (var key in object) {
-          has_keys = true;
-        }
-        return has_keys;
-      }*/
-
-    parse_pgn_header(header, [Map options]) {
-      var newline_char =
-          (options != null && options.containsKey("newline_char"))
-              ? options['newline_char']
-              : '\r?\n';
-      var header_obj = {};
-      var headers = header.split(newline_char);
-      var key = '';
-      var value = '';
-
-      for (var i = 0; i < headers.length; i++) {
-        RegExp keyMatch = new RegExp(r"^\[([A-Z][A-Za-z]*)\s.*\]$");
-        var temp = keyMatch.firstMatch(headers[i]);
-        if (temp != null) {
-          key = temp[1];
-        }
-        //print(key);
-        RegExp valueMatch = new RegExp(r'^\[[A-Za-z]+\s"(.*)"\]$');
-        temp = valueMatch.firstMatch(headers[i]);
-        if (temp != null) {
-          value = temp[1];
-        }
-        //print(value);
-        if (trim(key).length > 0) {
-          header_obj[key] = value;
-        }
-      }
-
-      return header_obj;
-    }
-
-    var newline_char = (options != null && options.containsKey("newline_char"))
-        ? options["newline_char"]
-        : '\r?\n';
-    //var regex = new RegExp(r'^(\[.*\]).*' + r'1\.'); //+ r"1\."); //+ mask(newline_char));
-
-    int indexOfMoveStart = pgn.indexOf(new RegExp(newline_char + r"1\."));
-
-    /* get header part of the PGN file */
-    String header_string = null;
-    if (indexOfMoveStart != -1) {
-      header_string = pgn.substring(0, indexOfMoveStart).trim();
-    }
-
-    /* no info part given, begins with moves */
-    if (header_string == null || header_string[0] != '[') {
-      header_string = '';
-    }
-
-    reset();
-
-    /* parse PGN header */
-    var headers = parse_pgn_header(header_string, options);
-    for (var key in headers.keys) {
-      set_header([key, headers[key]]);
-    }
-
-    /* delete header to get the moves */
-    var ms = pgn
-        .replaceAll(header_string, '')
-        .replaceAll(new RegExp(mask(newline_char)), ' ');
-
-    /* delete comments */
-    ms = ms.replaceAll(new RegExp(r"(\{[^}]+\})+?"), '');
-
-    /* delete move numbers */
-    ms = ms.replaceAll(new RegExp(r"\d+\."), '');
-
-    /* trim and get array of moves */
-    var moves = trim(ms).split(new RegExp(r"\s+"));
-
-    /* delete empty entries */
-    moves = moves.join(',').replaceAll(new RegExp(r",,+"), ',').split(',');
-    var move;
-
-    for (var half_move = 0; half_move < moves.length - 1; half_move++) {
-      move = get_move_obj(moves[half_move]);
-
-      /* move not possible! (don't clear the board to examine to show the
-         * latest valid position)
-         */
-      if (move == null) {
-        return false;
-      } else {
-        make_move(move);
-      }
-    }
-
-    /* examine last move */
-    move = moves[moves.length - 1];
-    if (POSSIBLE_RESULTS.contains(move)) {
-      if (!game.header.containsKey("Result")) {
-        set_header(['Result', move]);
-      }
-    } else {
-      var moveObj = get_move_obj(move);
-      if (moveObj == null) {
-        return false;
-      } else {
-        make_move(moveObj);
-      }
-    }
-    return true;
-  }
-
   /// The move function can be called with in the following parameters:
   /// .move('Nxb7')      <- where 'move' is a case-sensitive SAN string
   /// .move({ from: 'h7', <- where the 'move' is a move object (additional
@@ -1377,7 +1056,7 @@ class Chess {
   /// It returns true if the move was made, or false if it could not be.
   bool move(move) {
     Move move_obj = null;
-    List<Move> moves = generate_moves();
+    List<Move> moves = generateMoves();
 
     if (move is String) {
       /* convert the move string to a move object */
@@ -1416,12 +1095,6 @@ class Chess {
     return true;
   }
 
-  /// Takeback the last half-move, returning a move Map if successful, otherwise null.
-  undo() {
-    var move = undo_move();
-    return (move != null) ? make_pretty(move) : null;
-  }
-
   /// Returns the color of the square ('light' or 'dark'), or null if [square] is invalid
   String square_color(square) {
     if (SQUARES.containsKey(square)) {
@@ -1430,30 +1103,6 @@ class Chess {
     }
 
     return null;
-  }
-
-  getHistory([Map options]) {
-    List<Move> reversed_history = [];
-    var move_history = [];
-    var verbose = (options != null &&
-        options.containsKey("verbose") &&
-        options["verbose"] == true);
-
-    while (game.history.length > 0) {
-      reversed_history.add(undo_move());
-    }
-
-    while (reversed_history.length > 0) {
-      Move move = reversed_history.removeLast();
-      if (verbose) {
-        move_history.add(make_pretty(move));
-      } else {
-        move_history.add(move_to_san(move));
-      }
-      make_move(move);
-    }
-
-    return move_history;
   }
 
   // Constants/Class Variables
@@ -2101,6 +1750,4 @@ class Chess {
       {'square': SQUARES_H8, 'flag': BITS_KSIDE_CASTLE}
     ]
   };
-
-  static Uuid uuid = Uuid();
 }
